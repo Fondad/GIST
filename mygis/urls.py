edb7e62fbc1fa61fcedd6eef24852c9a0edd956d
Vsevolod Demidov <fondad@ukr.net>
2023-03-20 11:23:38 +0200

    mygis urls was created. Home views was added

--- mygis/urls.py
+++ mygis/urls.py
@@ -13,19 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
 
+
+# from django.contrib import admin
+# from django.urls import path
+
+from mygis import views
+from django.conf.urls import url
 '''
 urlpatterns = [
-    path('mygis/', include('mygis.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
 '''
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('mygis.urls')),
+    url(r'^$', views.home, name='home'),
 ]
 
